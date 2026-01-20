9d986f2e1ae186fc2068766b2508d26ddf6bbdfa
Muse <Mussiehailug@gmail.com>
2026-01-20 13:30:27 +0000

    TASK1:S1_VAR_09--checking the “undefined” from typeof

---
+++ js/stage_1/S1_VAR_09.js
@@ -0,0 +1,3 @@
+// Using typeof on an undeclared variable
+console.log(typeof someUndeclaredName); // "undefined"
+//In JavaScript, typeof never throws an error even if the variable doesn’t exist,it retuns ""undefined"

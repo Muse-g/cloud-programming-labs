d483e87a0cdb7c1f007b5a440198f359c263746a
Muse <Mussiehailug@gmail.com>
2026-01-20 12:53:23 +0000

    TASK1: S1_VAR_01 - var let const type inspection

---
+++ js/stage_1/S1_VAR_01.js
@@ -0,0 +1,10 @@
+// S1_VAR_01 — Declare & observe
+
+var city = "krakow";
+let year = 2026;
+const isOnline = true;
+
+console.log("name | value | typeof");
+console.log("city |", city, "|", typeof city);
+console.log("year |", year, "|", typeof year);
+console.log("isOnline |", isOnline, "|", typeof isOnline);

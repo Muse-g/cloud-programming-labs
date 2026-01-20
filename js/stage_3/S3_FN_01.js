b5336435f3153f8852c307aa15d6172b2b2c04d2
Muse <Mussiehailug@gmail.com>
2026-01-20 15:55:37 +0000

    TASK3:S3_FN_01 — Convert to arrows

---
+++ js/stage_3/S3_FN_01.js
@@ -0,0 +1,13 @@
+// Original functions (example)function, square(n) { return n * n; }
+// function isOdd(n) { return n % 2 !== 0; }, function greet(name) { return "Hello " + name; }
+
+// Arrow function versions
+const square = (n) => n * n;
+const isOdd = (n) => n % 2 !== 0;
+const greet = (name) => `Hello ${name}`;
+
+// Tests we done to make sure it works
+console.log(square(5)); // 25
+console.log(isOdd(5)); // true
+console.log(isOdd(4)); // false
+console.log(greet("Ala")); // "Hello Ala"

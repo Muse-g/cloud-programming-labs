1202028ba456d18f39fd37c040256a7a973a0dee
Muse <Mussiehailug@gmail.com>
2026-01-20 14:43:25 +0000

    TASK2:S2_ARR_02 — Deduplicate corected the title

--- js/stage_2/S2_ARR_02.js
+++ js/stage_2/S2_ARR_02.js
@@ -5,7 +5,7 @@ function unique(values) {
   }
   return result;
 }
-
+//
 // Test
 console.log(unique([1, 2, 1, 3, 2, 4])); // [1, 2, 3, 4]
 console.log(unique(["a", "b", "a", "c"])); // ["a", "b", "c"]

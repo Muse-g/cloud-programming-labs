664be1cbb372ad9b3bcc289cac126385d83b99d5
Muse <Mussiehailug@gmail.com>
2026-01-20 15:51:13 +0000

    S3_OBJ_06 — Group by /role in our case

---
+++ js/stage_3/S3_OBJ_06.js
@@ -0,0 +1,33 @@
+function groupBy(items, key) {
+  const result = {};
+
+  for (const item of items) {
+    const groupKey = item[key];
+    if (!(groupKey in result)) {
+      result[groupKey] = [];
+    }
+    result[groupKey].push(item);
+  }
+
+  return result;
+}
+
+// Test
+const users = [
+  { name: "Ala", role: "admin" },
+  { name: "Beki", role: "user" },
+  { name: "Ola", role: "admin" },
+];
+
+console.log(groupBy(users, "role"));
+/*
+  {
+    admin: [
+      { name: "Ala", role: "admin" },
+      { name: "Ola", role: "admin" }
+    ],
+    user: [
+      { name: "Beki", role: "user" }
+    ]
+  }
+  */
